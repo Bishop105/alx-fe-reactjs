2d348dab85fd10219fa385be83d2ca0771474087
Bismark Asuming <bismarkasuming3@gmail.com>
2025-10-30 08:29:58 +0000

    Created UserProfile component and used it in App component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -13,6 +14,11 @@ function App() {
   return (
     <>
       <div>
+          <UserProfile 
+                name="Alice" 
+                age="25" 
+                bio="Loves hiking and photography" 
+            />
          <Header />
          <MainContent />
          <Footer />
