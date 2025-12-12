de73e5e4c22d1d43096dfd25f2a440f2aba00044
Bismark Asuming <bismarkasuming3@gmail.com>
2025-12-12 07:41:03 +0000

    RegistrationForm.jsx

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 const RegistrationForm = () => {
   const [formData, setFormData] = useState({
-    username: '',
+  ["value={username}", "value={email}", "value={password}"]  username: '',
     email: '',
     password: '',
   });
